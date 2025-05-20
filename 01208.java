8815aeea06236bceff146d92a0b18486530e059c
nabiha175 <inwardoutwardsbilling1@gmail.com>
2025-05-20 12:41:50 +0000

    version

---
+++ 01208.java
@@ -0,0 +1,13 @@
+class Account
+{
+    private String name;
+    private int account bal;
+    private double balwuance;
+    private String addressString;
+    private String accountType;
+
+   public Account(String n,int number,double bal){
+         name = n;
+         accountNumber = number;
+         balance = bal;
+   }
