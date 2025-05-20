54c1d4ffabaf5dc2f5c0af5218c16252f9c0a13a
nabiha175 <inwardoutwardsbilling1@gmail.com>
2025-05-20 12:23:36 +0000

    version1

---
+++ Exc3.java
@@ -0,0 +1,7 @@
+for (int i = 1; i <= 10; i++) {
+        if (i == 5) {
+            break; // Exits the loop when i is 5
+        }
+        System.out.print(i + " ");
+    }
+    // Output: 1 2 3 4
